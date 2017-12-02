44c1f9095267636ca9f6146302c7e95aa6a60244
jerry <mowen0303@gmail.com>
2017-12-01 23:38:08 -0500

    0.0.1.13

--- admin/templete/_left.php
+++ admin/templete/_left.php
@@ -31,17 +31,19 @@ if(!$userModel->isAdminLogin()){
 		<header class="navTit">内容管理系统</header>
 		<article class="navCon">
 			<ul>
-				<li><a href="/admin/forum/" target="mainFrame">新鲜事</a></li>
-				<li><a href="/admin/guide/" target="mainFrame">资讯</a></li>
-                <li><a href="/admin/courseCode/index.php?s=listCourseCode&parent_id=0" target="mainFrame">科目代码</a ></li>
-				<li><a href="/admin/course/" target="mainFrame">课评</a></li>
-                <li><a href="/admin/professor/index.php?s=listProfessor" target="mainFrame">教授</a></li>
-				<li><a href="/admin/user/index.php?s=listUser&isAdmin=0" target="mainFrame">用户</a></li>
-				<li><a href="/admin/adCategory/" target="mainFrame">广告</a></li>
-                <li><a href ="/admin/eventCategory/" target="mainFrame">活动</a></li>
+				<li><a href="/admin/adCategory/" target="mainFrame">广告管理</a></li>
+				<li><a href ="/admin/eventCategory/" target="mainFrame">活动管理</a></li>
+				<li><a href="/admin/forum/" target="mainFrame">同学圈</a></li>
+				<li><a href="/admin/guide/" target="mainFrame">文章</a></li>
 				<li><a href="/admin/book/index.php?s=listBook" target="mainFrame">二手书</a ></li>
-				<li><a href="/admin/map/" target="mainFrame">地图</a ></li>
+				<li><a href="/admin/course/" target="mainFrame">课评V1</a></li>
+				<li><a href="/admin/courseQuestion/" target="mainFrame">问答系统</a></li>
+                <li><a href="/admin/professor/index.php?s=listProfessor" target="mainFrame">教授管理</a></li>
+				<li><a href="/admin/user/index.php?s=listUser&isAdmin=0" target="mainFrame">用户管理</a></li>
+				<li><a href="/admin/map/" target="mainFrame">地图管理</a ></li>
+				<li><a href="/admin/courseCode/index.php?s=listCourseCode&parent_id=0" target="mainFrame">科目管理</a ></li>
                 <li><a href="/admin/transaction/index.php" target="mainFrame">积分记录</a ></li>
+				<li><a href="/admin/msg/index.php" target="mainFrame">小纸条</a></li>
 			</ul>
 		</article>
 	</section>
@@ -49,7 +51,6 @@ if(!$userModel->isAdminLogin()){
 		<header class="navTit">系统</header>
 		<article class="navCon">
 			<ul>
-				<li><a href="/admin/msg/index.php" target="mainFrame">小纸条</a></li>
 				<li><a href="/admin/user/index.php?s=listUser&isAdmin=1" target="mainFrame">管理员</a></li>
 				<li><a href="/admin/user/index.php?s=listUserClass" target="mainFrame">权限配置</a></li>
 				<li><a href="/admin/statistics/index.php?s=listStatistics" target="mainFrame">Dashboard</a></li>
