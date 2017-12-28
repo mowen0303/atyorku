0cbe890c32c10dbc36af315e527a71498bfbd850
xin <chenxinrong123@.com>
2017-12-28 16:39:37 -0500

    2.0.2.2

--- admin/xmlParser/parse_prof.php
+++ admin/xmlParser/parse_prof.php
@@ -8,18 +8,26 @@ $profs_check = [];
 $profs = [];
 foreach ($json_data["response"]["docs"] as $prof_profile)
 {
-    $prof_full_name = $prof_profile["teacherlastname_t"]." ".$prof_profile["teacherfirstname_t"];
-    if (!in_array("$prof_full_name",$profs_check)){
+    $prof_full_name = $prof_profile["teacherfirstname_t"]." ".$prof_profile["teacherlastname_t"];
+    if (!in_array($prof_full_name,$profs_check)){
         array_push($profs_check,$prof_full_name);
         $prof = [];
         $prof["view_count"] = 0;
-        $prof["firstname"] =$prof_profile["teacherfirstname_t"]?$prof_profile["teacherfirstname_t"]:"";
-        $prof["lastname"] = $prof_profile["teacherlastname_t"]?$prof_profile["teacherlastname_t"]:"";
-        array_push($profs,$prof);
+        $prof_full_name = explode(" ",$prof_full_name);
+        //$prof["firstname"] =$prof_profile["teacherfirstname_t"]?$prof_profile["teacherfirstname_t"]:"";
+        //$prof["lastname"] = $prof_profile["teacherlastname_t"]?$prof_profile["teacherlastname_t"]:"";
+        $count = count($prof_full_name);
+        if ($count >= 2){
+            $prof["firstname"] = implode(" ",array_slice($prof_full_name,0,$count-1));
+            $prof["lastname"] = $prof_full_name[$count-1];
+            array_push($profs,$prof);
+        }
+        if ($count == 1){
+            $prof["lastname"] = $prof_full_name[0];
+            $prof["firstname"] = "";
+        }
     }
-    else{
 
-    }
 
 }
 foreach($profs as $prof){
