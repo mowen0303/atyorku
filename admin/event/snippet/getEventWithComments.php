2359305f0c4bc748bb7077f1d3b41a0edad1cd15
xin <chenxinrong123@.com>
2018-01-02 03:13:01 -0500

    2.0.37

--- admin/event/snippet/getEventWithComments.php
+++ admin/event/snippet/getEventWithComments.php
@@ -113,157 +113,23 @@ $sponsor =$userModel->getProfileOfUserById($event["sponsor_user_id"]);
 </article>
 <article class="mainBox">
     <header><h2>用户评论</h2></header>
-    <form action="/admin/comment/commentController.php?action=deleteParentComment" method="post">
-        <section>
-            <table class="tab">
-                <thead>
-                <tr>
-                    <th width="21px"><input id="cBoxAll" type="checkbox"></th>
-                    <th>评论ID</th>
-                    <th>父ID</th>
-                    <th>头像</th>
-                    <th>用户名</th>
-                    <th>性别</th>
-                    <th>内容</th>
-                    <th>时间</th>
-                    <th>操作</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                $comments = $commentModel->getComments("event",$event_id);
-                $parent = 0;
-                foreach ($comments as $comment) {
-
-                    if ($comment["r_id"]==null){
-                        unset($l_id);
-                        $parent = 0;
-                        $sender=$userModel->getProfileOfUserById($comment["l_sender_id"]);
-                        ?>
-                        <tr>
-                            <td><input type="checkbox" class="cBox" name="id[]" value="<?php echo $comment['l_id']?>"></td>
-                            <td><?php echo $comment['l_id']?></td>
-                            <td><?php echo $comment['l_parent_id']?></td>
-                            <td><img width="36" height="36" src="<?php echo $sender['img'] ?>"></td>
-                            <td><?php echo $sender['name']?></td>
-                            <td><?php echo $sender['gender']?></a></td>
-                            <td><?php echo $comment['l_comment'] ?></td>
-                            <td><?php echo BasicTool::translateTime($comment['l_time']) ?></td>
-                            <td><a href="index.php?s=addReply&parent_id=<?php echo $comment["l_id"] ?>&event_id=<?php echo $event_id?>">回复</a></td>
-                        </tr>
-                    <?php }
-                    else if ($parent == 0 && $comment["r_id"] != null ){
-
-                        $parent = 1;
-                        $sender = $userModel->getProfileOfUserById($comment["l_sender_id"]);
-                        $l_id = $comment["l_id"];
-
-                     ?>
-                    <tr>
-                        <td><input type="checkbox" class="cBox" name="id[]" value="<?php echo $comment['l_id']?>"></td>
-                        <td><?php echo $comment['l_id']?></td>
-                        <td><?php echo $comment['l_parent_id']?></td>
-                        <td><img width="36" height="36" src="<?php echo $sender['img'] ?>"></td>
-                        <td><?php echo $sender['name']?></td>
-                        <td><?php echo $sender['gender']?></a></td>
-                        <td><?php echo $comment['l_comment'] ?></td>
-                        <td><?php echo BasicTool::translateTime($comment['l_time']) ?></td>
-                        <td><a href="index.php?s=addReply&parent_id=<?php echo $comment["l_id"] ?>&event_id=<?php echo $event_id?>">回复</a></td>
-                    </tr>
-                        <?php
-
-                            $sender = $userModel->getProfileOfUserById($comment["r_sender_id"]);
-                        ?>
-                    <tr id="comment_<?php echo $comment["r_id"]?>">
-                        <td>&nbsp;&nbsp;</td>
-                        <td><?php echo $comment['r_id']?></td>
-                        <td><?php echo $comment['r_parent_id']?></td>
-                        <td><img width="36" height="36" src="<?php echo $sender['img'] ?>"></td>
-                        <td><?php echo $sender['name']?></td>
-                        <td><?php echo $sender['gender']?></a></td>
-                        <td><?php echo $comment['r_comment'] ?></td>
-                        <td><?php echo BasicTool::translateTime($comment['r_time']) ?></td>
-                        <td><span style="cursor:pointer;" onclick="<?php echo "removeComment({$comment['r_id']})" ?>">删除</span></td>
-                    </tr>
-                <?php
-                }
-                else if ($parent == 1 && $comment["r_id"] != null && $comment["l_id"] == $l_id){
-                        $sender = $userModel->getProfileOfUserById($comment["r_sender_id"]);
-                ?>
-                    <tr id="comment_<?php echo $comment["r_id"]?>">
-                        <td>&nbsp;&nbsp;</td>
-                        <td><?php echo $comment['r_id']?></td>
-                        <td><?php echo $comment['r_parent_id']?></td>
-                        <td><img width="36" height="36" src="<?php echo $sender['img'] ?>"></td>
-                        <td><?php echo $sender['name']?></td>
-                        <td><?php echo $sender['gender']?></a></td>
-                        <td><?php echo $comment['r_comment'] ?></td>
-                        <td><?php echo BasicTool::translateTime($comment['r_time']) ?></td>
-                        <td><span style="cursor:pointer;" onclick="<?php echo "removeComment({$comment['r_id']})" ?>">删除</span></td>
-
-                    </tr>
-                <?php }
-                else if ($parent == 1 && $comment["r_id"] != null && $comment["l_id"] != $l_id){
-                    $l_id = $comment["l_id"];
-                    $parent = 1;
-                    $sender = $userModel->getProfileOfUserById($comment["l_sender_id"]);
-                    $l_id = $comment["l_id"];
-
-                    ?>
-                    <tr>
-                        <td><input type="checkbox" class="cBox" name="id[]" value="<?php echo $comment['l_id']?>"></td>
-                        <td><?php echo $comment['l_id']?></td>
-                        <td><?php echo $comment['l_parent_id']?></td>
-                        <td><img width="36" height="36" src="<?php echo $sender['img'] ?>"></td>
-                        <td><?php echo $sender['name']?></td>
-                        <td><?php echo $sender['gender']?></a></td>
-                        <td><?php echo $comment['l_comment'] ?></td>
-                        <td><?php echo BasicTool::translateTime($comment['l_time']) ?></td>
-                        <td><a href="index.php?s=addReply&parent_id=<?php echo $comment["l_id"] ?>&event_id=<?php echo $event_id?>">回复</a></td>
-                    </tr>
-                    <?php
-
-                    $sender = $userModel->getProfileOfUserById($comment["r_sender_id"]);
-                    ?>
-                    <tr id="comment_<?php echo $comment["r_id"]?>">
-                        <td>&nbsp;&nbsp;</td>
-                        <td><?php echo $comment['r_id']?></td>
-                        <td><?php echo $comment['r_parent_id']?></td>
-                        <td><img width="36" height="36" src="<?php echo $sender['img'] ?>"></td>
-                        <td><?php echo $sender['name']?></td>
-                        <td><?php echo $sender['gender']?></a></td>
-                        <td><?php echo $comment['r_comment'] ?></td>
-                        <td><?php echo BasicTool::translateTime($comment['r_time']) ?></td>
-                        <td><span style="cursor:pointer;" onclick="<?php echo "removeComment({$comment['r_id']})" ?>">删除</span></td>
-                    </tr>
-                    <?php
-                }
-                }
-                ?>
-                </tbody>
-            </table>
-            <?php echo $commentModel->echoPageList()?>
+    <!--评论组件 S-->
+    <!--
+    data-category 产品数据库表名
+    data-production-id 产品ID（文章、二手书、分享、同学圈）
+    data-receiver-id 产品作者ID
+    -->
+    <div id="commentComponent"
+         data-category="event"
+         data-production-id="<?php echo $event_id?>"
+         data-receiver-id="<?php echo $event["sponsor_user_id"]?>"
+         style="background: transparent">
+        <section class="textAreaContainer">
+            <textarea name="comment" placeholder="说两句吧..."></textarea>
+            <div id="commentButton">评论</div>
         </section>
-        <footer class="buttonBox">
-            <input type="submit" value="删除" class="btn" onclick="return confirm('确认删除吗?')">
-        </footer>
-    </form>
-</article>
-<article class="mainBox">
-    <header><h2>添加评论</h2></header>
-    <section class="formBox">
-        <form action="/admin/comment/commentController.php?action=addComment" method="post">
-            <div>
-                <input type="hidden" name="parent_id" value="0"/>
-                <input type="hidden" name="sender_id" value="<?php echo $userModel->userId?>"/>
-                <input type="hidden" name="receiver_id" value="<?php echo $event["sponsor_user_id"]?>"/>
-                <input type="hidden" name="section_name" value="event"/>
-                <input type="hidden" name="section_id" value="<?php echo $event["id"]?>"/>
-                <textarea value="" name="comment" placeholder="输入留言" class="input input-textarea"></textarea>
-            </div>
-            <div>
-                <input type="submit" value="提交" title="提交" class="btn btn-center">
-            </div>
-        </form>
-    </section>
+        <section id="commentListContainer"></section>
+        <section id="loadMoreButton">点击加载更多</section>
+    </div>
+    <!--评论组件 E-->
 </article>
