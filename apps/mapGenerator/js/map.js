508fda5f0dae24aa3fd6c8f64a9f19b61f882638
Jerry Zhang <mowen0303@gmail.com>
2018-02-26 18:21:11 -0500

    2.b.11(增加地图坐标生成工具)

---
+++ apps/mapGenerator/js/map.js
@@ -0,0 +1,119 @@
+var polygon;
+var polygonCoordinate = [];
+var map;
+var mapCenter;
+var editable = true;
+var mapListener;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: {
+            lat: 43.771,
+            lng: -79.500
+        },
+        mapTypeId: 'terrain'
+    });
+
+    mapListener = map.addListener('click', function (e) {
+        clickHandler(e)
+    });
+    map.addListener('drag', function () {
+        //console.log(1)
+    });
+
+    polygon = new google.maps.Polygon({
+        paths: polygonCoordinate,
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 1,
+        fillColor: '#FF0000',
+        fillOpacity: 0.3,
+        editable: editable,
+        draggable: false,
+    });
+
+    polygon.addListener('dragend', function () {
+        //console.log('Vertex removed from inner path.');
+    });
+
+    // polygon.addListener('click', function () {
+    //     editable = !editable;
+    //     polygon.setEditable(editable);
+    //     if (editable) {
+    //         mapListener = map.addListener('click', function (e) {
+    //             clickHandler(e)
+    //         });
+    //     } else {
+    //         //结束编辑
+    //         google.maps.event.removeListener(mapListener);
+    //     }
+    //
+    // });
+
+    polygon.addListener('click', showArrays);
+    //infoWindow = new google.maps.InfoWindow;
+}
+
+function showArrays(event) {
+        // Since this polygon has only one path, we can call getPath() to return the
+        // MVCArray of LatLngs.
+        var vertices = this.getPath();
+
+        // var contentString = '<b>点击点坐标: </b><br><br>' + event.latLng.lat() + ',' + event.latLng.lng() +
+        //     '<br><br><br><br><b>形状坐标: </b><br>';
+        var contentString = "";
+        // Iterate over the vertices.
+        for (var i =0; i < vertices.getLength(); i++) {
+          var xy = vertices.getAt(i);
+          contentString += xy.lat() + ',' + xy.lng() + ';';
+        }
+
+        // Replace the info window's content and position.
+        console.log(contentString)
+        //infoWindow.setPosition(event.latLng);
+
+        document.execCommand('copy');
+      }
+
+function drawPolygon(polygon) {
+    polygon.setMap(map);
+}
+
+function removePloygon() {
+    polygonCoordinate = [];
+    polygon.setMap(null);
+}
+
+function clickHandler(e) {
+    polygonCoordinate.push(e.latLng.toJSON());
+    polygon.setPaths(polygonCoordinate);
+    //listener
+    addEditListener();
+    // polygon.setMap(null);
+    drawPolygon(polygon);
+}
+
+function addEditListener() {
+    polygon.getPaths().forEach(function (path, index) {
+        google.maps.event.addListener(path, 'insert_at', function () {
+            //console.log("insert_at");
+        });
+        google.maps.event.addListener(path, 'remove_at', function () {
+            //console.log("remove_at");
+        });
+        google.maps.event.addListener(path, 'set_at', function () {
+            //console.log("set_at");
+            //console.log(polygonCoordinate);
+            var coordinateArr = polygon.getPaths().getArray()[0].getArray();
+            polygonCoordinate = [];
+            coordinateArr.forEach(function (element) {
+                polygonCoordinate.push(element.toJSON());
+            })
+            //console.log(polygonCoordinate);
+            polygon.setPaths(polygonCoordinate);
+            addEditListener();
+            drawPolygon(polygon);
+        });
+    });
+}
