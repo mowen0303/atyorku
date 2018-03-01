f2a79ee53dc0154603afb5d114ed4c8f5cc529a6
Jerry Zhang <mowen0303@gmail.com>
2018-03-01 17:18:56 -0500

    2.0.b.16

--- apps/guide/share.php
+++ apps/guide/share.php
@@ -14,7 +14,7 @@ $signPackage = $jssdk->GetSignPackage();
     <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport"/>
     <meta charset="UTF-8">
     <title><?php echo $arr['title'] ?></title>
-    <link href="/apps/guide/css.css?1215131" rel="stylesheet" type="text/css">
+    <link href="/apps/guide/css.css?1215135" rel="stylesheet" type="text/css">
     <style type="text/css">
         #coverImgBox {
             position: absolute;
