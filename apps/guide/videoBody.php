03fa74efadd753f6d80c74603323c53e8d13c4ce
jerry <mowen0303@gmail.com>
2018-09-24 02:08:43 -0400

    2.5.0.22

--- apps/guide/videoBody.php
+++ apps/guide/videoBody.php
@@ -1,59 +1,61 @@
-<article id="container">
-    <div>
-        <!-- <div id="coverImgBox"><img style="width: 100%; height: auto" src="<?php echo $arr['cover'] ?>"></div> -->
-        <div class="wrapper">
-            <div class="videoBox">
-                <!--放视频 S-->
-                <!--
-                放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频
-                放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频
-                放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频
-                放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频 放视频
-                -->
-            </div>
-            <div class="infoBox">
-                <h6 class="title"><?php echo $arr["title"]?></h6>
-                <?php
-                if ($arr["introduction"])
-                    echo "<p class='intro'>{$arr['introduction']}</p>";
+<article id="container" class="videoContainer">
+    <div id="videoBox">
+        <div class="aspectration" style="position: relative; height: 0; width: 100%; padding-top: 56.25%;" data-ratio="16:9">
+            <?php
+            if($arr['video_vendor']=="youtube"){
                 ?>
-                <div>
-                    <span class="subtitle"><?php echo BasicTool::translateTime($arr["time"])?></span>
-                    <span class="subtitle"><?php echo $arr["category_title"]?></span>
-                </div>
+                <iframe style="position: absolute; left:0;top:0;width:100%;height:100%" src="<?php echo $arr["video_source_url"]?>?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media"></iframe>
+                <?php
+            }else {
+                ?>
+                <iframe style="position: absolute; left:0;top:0;width:100%;height:100%" src="<?php echo $arr["video_source_url"]?>" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen=""></iframe>
+                <?php
+            }
+            ?>
+        </div>
+    </div>
+    <div id="videoBody">
+        <div class="videoTitleBox">
+            <div class="videoTitle"><?php echo $arr["title"]?></div>
+            <div class="videoSubtitle">
+                <span><?php echo BasicTool::translateTime($arr["time"])?></span>
+                <span><?php echo $arr["category_title"]?></span>
+                <span>
+                    <?php
+                    if($arr['view_no']>=500){
+                        echo '浏览量：'.$arr['view_no'];
+                    }
+                    ?>
+                </span>
             </div>
+            <section class="videoContext">
+                <?php echo $arr['content']; ?>
+            </section>
         </div>
 
-        <section class="context">
-            <p style="margin-top:2em"><img src="/resource/img/gzhqr.jpg" alt=""><p><p class="copyDoc"></p>
-        </section>
-        <?php
-        if($arr['view_no']>=500){
-            echo '<div class="readCount"><em></em><span>浏览量：'.$arr['view_no'].'</span><em></em></div>';
-        }
-        ?>
-
+        <!--评论组件 S-->
+        <!--
+        data-category 产品数据库表名
+        data-production-id 产品ID（文章、二手书、分享、同学圈）
+        data-receiver-id 产品作者ID
+        -->
+        <script type="text/javascript" src="/admin/resource/js/component.js"></script>
+        <div id="commentComponent"
+             style="margin-top: 0"
+             data-category="guide"
+             data-production-id="<?php echo $arr['id']; ?>"
+             data-receiver-id="<?php echo $arr['uid']; ?>">
+            <header><span>用户评论（<?php echo $arr['count_comments']; ?>）</span></header>
+            <section id="commentListContainer"></section>
+            <section id="loadMoreButton">点击加载更多评论</section>
+            <section class="textAreaContainer">
+                <textarea name="comment" placeholder="说两句吧..."></textarea>
+                <div id="commentButton">评论</div>
+            </section>
+        </div>
+        <!--评论组件 E-->
+        <div style="padding:2em 0; background: #f2f2f2"><img src="/resource/img/gzhqr.jpg" alt=""></div>
     </div>
-    <!--评论组件 S-->
-    <!--
-    data-category 产品数据库表名
-    data-production-id 产品ID（文章、二手书、分享、同学圈）
-    data-receiver-id 产品作者ID
-    -->
-    <script type="text/javascript" src="/admin/resource/js/component.js"></script>
-    <div id="commentComponent"
-         data-category="guide"
-         data-production-id="<?php echo $arr['id']; ?>"
-         data-receiver-id="<?php echo $arr['uid']; ?>">
-        <header><span>用户评论（<?php echo $arr['count_comments']; ?>）</span></header>
-        <section id="commentListContainer"></section>
-        <section id="loadMoreButton">点击加载更多评论</section>
-        <section class="textAreaContainer">
-            <textarea name="comment" placeholder="说两句吧..."></textarea>
-            <div id="commentButton">评论</div>
-        </section>
-    </div>
-    <!--评论组件 E-->
 </article>
 
 <?php
